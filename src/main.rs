7c20e5f85c75be3aac7da031701f0e8f95e816a2
Vitaly _Vi Shukela <vi0oss@gmail.com>
2017-12-10 03:16:34 +0300

    First step

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
+#![allow(unused_mut)]
+#![allow(unused_variables)]
 extern crate dns_parser;
 
-use std::net::UdpSocket;
+use std::net::{UdpSocket, SocketAddr};
+use dns_parser::Packet;
+use std::collections::HashMap;
 
-fn run() -> std::io::Result<()> {
-    let s = UdpSocket::bind("127.0.0.1:3553")?;
+fn run() -> Result<(),Box<std::error::Error>> {
+
+    let s = UdpSocket::bind("0.0.0.0:3553")?;
+    let upstream : SocketAddr = "8.8.8.8:53".parse()?;
+    
+    let mut r2a : HashMap<u16, SocketAddr> = HashMap::new();
     
     let mut buf = [0; 1600];
     loop {
         let (amt, src) = s.recv_from(&mut buf)?;
         let buf = &mut buf[..amt];
-        s.send_to(&buf, &src)?;
+        let p = Packet::parse(buf)?;
+        
+        if src == upstream {
+            println!("reply: {:?}", p);
+            
+            if let Some(ca) = r2a.get(&p.header.id) {
+                s.send_to(&buf, &ca)?;
+            }
+            
+        } else {
+            println!("request {:?}", p);
+            
+            r2a.insert(p.header.id, src);
+            
+            s.send_to(&buf, &upstream)?;
+        }
     }
 }
 
 fn main() {
-    run().unwrap()
+    if let Err(e) = run() {
+        eprintln!("error: {}", e);
+        std::process::exit(1);
+    }
 }
